346f171826f1153f9928787b0d889364f43abdbe
TejwantKaur <kaurtejwant998@gmail.com>
2024-01-03 10:23:21 +0530

    count of users

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
+const express = require("express");
+const app = express();
+const path = require("path");
 
+app.set ("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"))
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -17,23 +22,55 @@ let getRandomUser = () => {
     ];
 };
 
-let q = "INSERT INTO user(id, username, email, password) VALUES?";
-let data = [];
+app.get("/", (req, res)=>{
+    // res.send("Welcome to home page");
 
-for(let i=1; i<=100; i++){
-    data.push(getRandomUser());
-}
+    let q = `SELECT count(*) FROM user`;
+    try{
+        connection.query(q, (err, result)=>{
+            // result is basicalyy the result we get from query q
+            if(err) throw (err);
+            console.log(result[0]["count(*)"]);
+            
+            let count = result[0]["count(*)"];
+            res.render("home.ejs", {count});
+        });
+    } catch (err){
+        console.log(err);
+        res.send("Some error in DataBase");
+    }
+})
 
-try{
-    connection.query(q,[data], (err, result) =>{
-        if (err) throw err;
-        console.log(result);
-    });
-}catch(err){
-    console.log(err);
-};
+app.listen("8080", ()=>{
+    console.log("Server Started at port 8080");
+})
 
-connection.end();
+
+
+
+
+
+
+
+
+
+// let q = "INSERT INTO user(id, username, email, password) VALUES?";
+// let data = [];
+
+// for(let i=1; i<=100; i++){
+//     data.push(getRandomUser());
+// }
+
+// try{
+//     connection.query(q,[data], (err, result) =>{
+//         if (err) throw err;
+//         console.log(result);
+//     });
+// }catch(err){
+//     console.log(err);
+// };
+
+// connection.end();
 
 // let getRandomUser = () => {
 //     return {
